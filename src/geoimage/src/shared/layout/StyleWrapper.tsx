65d23e168ff028958cd6d0797126bfe59b238977
Lukas Bares <47695733+barluq@users.noreply.github.com>
2025-03-25 22:52:39 +0100

    fix review

--- src/geoimage/src/shared/layout/StyleWrapper.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import {enumCombineValuesToString} from "@geoimage/shared/helpers/code.formating"
-export const StylesWrapper = ({ children }) => {
-    return (<div className="">{children}</div>)
-}
