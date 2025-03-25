40bad970b7f82ea51fc422cc0ed251f45493a014
Lukas Bares <47695733+barluq@users.noreply.github.com>
2025-03-25 11:38:07 +0100

    rollup works for code

--- src/geoimage/rollup.config.js
+++ src/geoimage/rollup.config.js
@@ -2,6 +2,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+
 import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
@@ -13,6 +15,20 @@ export default {
     { file: pkg.main, format: 'cjs', sourcemap: true },
     { file: pkg.module, format: 'esm', sourcemap: true }
   ],
-  external: [...Object.keys(pkg.peerDependencies || {})],
-  plugins: [json(), resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
+  external: [
+    ...Object.keys(pkg.peerDependencies || {}),
+    'react', 'react-dom'
+  ],
+  plugins: [
+    json(),
+    postcss({
+      extract: true, // or false to inline styles
+      modules: true, // if you use CSS modules
+      minimize: true,
+      sourceMap: true
+    }),
+    resolve({ extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx'] }),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' })
+  ]
 };
