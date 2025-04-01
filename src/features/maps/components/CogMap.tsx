8ffabb288078776ecdf695a49d6126b253c2f8df
Lukas Bares <47695733+barluq@users.noreply.github.com>
2025-04-01 09:48:17 +0200

    flexible deck map component for COGs

--- src/features/maps/components/CogMap.tsx
+++ src/features/maps/components/CogMap.tsx
@@ -1,27 +1,74 @@
 "use client"
 
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { DeckGL } from "@deck.gl/react";
 import { defaultMapState, defaultMapView } from "../logic/map.defaults";
-import { createOpenstreetMap } from "../logic/map.layers.basemaps";
+import { createOpenstreetMap } from "../logic/layers.basemaps";
+import "../maps.css";
 
+/**
+ * CogMap map component (client side)
+ * This component is used to render the map with DeckGL
+ * It's the DeckGL map wrapper with dynamic DeckGL map size
+ * @returns {JSX.Element} Wrapped map component
+ */
 export const CogMap = () => {
 
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    const [dimensions, setDimensions] = useState({ width: '100%', height: '100%' });
+
+    // Capture the dimensions of the wrapper and make DeckGL Map flexible by JS
+    useEffect(() => {
+        if (!wrapperRef.current) return;
+
+        /**
+         * Update the dimensions of the DeckGL map
+         * @returns {void}
+         */
+        const updateDimensions = () => {
+            if (wrapperRef.current) {
+                const { width, height } = wrapperRef.current.getBoundingClientRect();
+                setDimensions({ width: `${width}px`, height: `${height}px` });
+            }
+        };
+
+        // Set dimensions by the wrapper
+        // This will be called once when the component mounts
+        updateDimensions();
+
+        // Also update when window resizes
+        window.addEventListener('resize', updateDimensions);
+
+        // Returning a React cleanup function
+        return () => window.removeEventListener('resize', updateDimensions);
+    }, []);
+
+    // Style that will exactly match the wrapper's dimensions
+    const mapStyle = {
+        position: 'absolute',
+        width: dimensions.width,
+        height: dimensions.height,
+        top: '0px',
+        left: '0px'
+    };
+
+    // list of layers to be rendered
+    // TODO: Now its harcoded, but later might be dynamic
     const layers = [
         createOpenstreetMap()
-    ]
+    ];
 
     return (
-        <section className="cog-map">
+        <section className="cog-map-wrapper" ref={wrapperRef}>
             <DeckGL
                 views={defaultMapView()}
                 initialViewState={defaultMapState()}
                 layers={layers}
-                style={{ width: "100vw", height: "50vh", display: "flex" }}
+                style={mapStyle}
                 controller={true}
-            >
-            </DeckGL>
-
+                width={dimensions.width}
+                height={dimensions.height}
+            />
         </section>
     )
 }
