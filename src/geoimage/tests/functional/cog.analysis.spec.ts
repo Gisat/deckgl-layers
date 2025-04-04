c9f518679f06d6fce8b04121d4a30a8827224a6c
Lukas Bares <47695733+barluq@users.noreply.github.com>
2025-04-04 16:15:17 +0200

    setup vitest

--- src/geoimage/tests/functional/cog.analysis.spec.ts
+++ src/geoimage/tests/functional/cog.analysis.spec.ts
@@ -1,6 +1,6 @@
 import { sortStringArray } from '@geoimage/shared/helpers/code.formating';
 import { cogFromFile } from '@geoimage/shared/integration/sources.read';
-import { describe, expect } from '@jest/globals';
+import { beforeAll, describe, expect, it } from 'vitest';
 import { useTestCogFile } from 'tests/tools/fixtures.files';
 
 let testCogFile: any
@@ -12,14 +12,14 @@ describe('COG loading and analysis', () => {
 
     });
 
-    test('Sorts strings in an array alphabetically', () => {
+    it('Sorts strings in an array alphabetically', () => {
         const unsortedArray = ['c', 'a', 'b'];
         const sortedArray = sortStringArray(unsortedArray);
         expect(sortedArray).toEqual(['a', 'b', 'c']);
     });
     
 
-    test('Do we have COG from file', async () => {
+    it('Do we have COG from file', async () => {
         const imageCount = await testCogFile.getImageCount();
         expect(imageCount).toBeGreaterThan(0);
     });
