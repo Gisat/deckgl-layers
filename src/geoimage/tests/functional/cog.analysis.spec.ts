0623f5acc6920e6c4bf93ef8c9d63153ffc3311d
Lukas Bares <47695733+barluq@users.noreply.github.com>
2025-04-04 16:42:36 +0200

    cog from url

--- src/geoimage/tests/functional/cog.analysis.spec.ts
+++ src/geoimage/tests/functional/cog.analysis.spec.ts
@@ -1,26 +1,26 @@
-import { sortStringArray } from '@geoimage/shared/helpers/code.formating';
-import { cogFromFile } from '@geoimage/shared/integration/sources.read';
+import { cogFromFile, cogFromUrl } from '@geoimage/shared/integration/sources.read';
 import { beforeAll, describe, expect, it } from 'vitest';
-import { useTestCogFile } from 'tests/tools/fixtures.files';
+import { useTestCogFile, useTestCogUrl } from '@test/tools/fixtures.cogs';
+import GeoTIFF from 'geotiff';
 
-let testCogFile: any
+let testCogFile: GeoTIFF
+let testCogSource: GeoTIFF
 
 describe('COG loading and analysis', () => {
 
     beforeAll(async () => {
         testCogFile = await cogFromFile(useTestCogFile())
+        testCogSource = await cogFromUrl(useTestCogUrl())
 
     });
 
-    it('Sorts strings in an array alphabetically', () => {
-        const unsortedArray = ['c', 'a', 'b'];
-        const sortedArray = sortStringArray(unsortedArray);
-        expect(sortedArray).toEqual(['a', 'b', 'c']);
-    });
-    
-
-    it('Do we have COG from file', async () => {
+    it('COG from file', async () => {
         const imageCount = await testCogFile.getImageCount();
+        expect(imageCount).toBeGreaterThan(0);
+    });
+
+    it('COG from URL', async () => {
+        const imageCount = await testCogSource.getImageCount();
         expect(imageCount).toBeGreaterThan(0);
     });
 
--- src/geoimage/tests/tools/fixtures.files.ts
+++
@@ -1,7 +0,0 @@
-import path from "path"
-
-export enum TEST_FILES {
-  DEM_COG = 'DEM_COP30_float32_wgs84_deflate_cog_float32.tif',
-}
-
-export const useTestCogFile = () => path.join(process.cwd(), "tests", 'fixtures', TEST_FILES.DEM_COG)
