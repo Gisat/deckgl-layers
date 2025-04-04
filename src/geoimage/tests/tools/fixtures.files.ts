6cd32c7dda2b4e0461862859fab391ed13e92957
Lukas Bares <47695733+barluq@users.noreply.github.com>
2025-04-04 16:37:21 +0200

    read file tiff works

--- src/geoimage/tests/tools/fixtures.files.ts
+++ src/geoimage/tests/tools/fixtures.files.ts
@@ -1,7 +1,7 @@
 import path from "path"
 
 export enum TEST_FILES {
-  DEM_COG = 'test_image.jpg',
+  DEM_COG = 'DEM_COP30_float32_wgs84_deflate_cog_float32.tif',
 }
 
-export const useTestCogFile = () => path.join(__dirname, '..', '..', 'fixtures', TEST_FILES.DEM_COG)
+export const useTestCogFile = () => path.join(process.cwd(), "tests", 'fixtures', TEST_FILES.DEM_COG)
