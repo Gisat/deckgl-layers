30a808d303402791cfcab4c3f75d605a2116eaa6
Lukas Bares <barluq697@gmail.com>
2025-05-30 17:20:07 +0200

    remove geoimage folder

--- src/geoimage/src/shared/helpers/code.types.ts
+++
@@ -1,6 +0,0 @@
-type Nullable<T> = T | null;
-type Unsure<T> = T | undefined;
-type NullablePromise<T> = Promise<T | null>;
-type UnsurePromise<T> = Promise<T | undefined>;
-
-export type { Nullable, Unsure, NullablePromise, UnsurePromise };
