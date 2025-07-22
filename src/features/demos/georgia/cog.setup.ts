bc7dea7c8327a1c9760d4f60858057c4d3b2df1d
Lukas Bares <barluq697@gmail.com>
2025-07-22 14:01:39 +0200

    georgia feature

---
+++ src/features/demos/georgia/cog.setup.ts
@@ -0,0 +1,30 @@
+import { RenderByValueDecider } from "geoimage-dev";
+
+
+// URL to the COG image for the demo
+export const useTestCogUrl = () => "https://gisat-gis.eu-central-1.linodeobjects.com/bsadri/test_raster/COG/LC_2021_all_Georgia_WEST3940_ZOOM6_test1_defl_COG256.tif"
+
+// Function to create a map for rendering RGBA colors based on pixel values for demo COG
+
+// TODO: more precise styling for later (maybe some specific demo, like specified data with color palethe?)
+export const useTestRenderingMapLogic = ({debugMode = false}: {debugMode?: boolean}): RenderByValueDecider => {
+
+    const randomR = Math.floor(Math.random() * 255);
+    const randomG = Math.floor(Math.random() * 255);
+    const randomB = Math.floor(Math.random() * 255);
+    const alpha = 170;
+
+    const decider = new Map<number | "unknown", [number, number, number, number]>();
+
+    if (debugMode) {
+        decider.set("unknown", [randomR, randomG, randomB, alpha]); // Unknown value
+        decider.set(0, [0, 0, 0, alpha]); // Empty pixels value
+        decider.set(255, [50, 50, 50, alpha]); // Outer box value
+    }
+
+    decider.set(11, [255, 0, 0, 255]); // Red for 11
+    decider.set(12, [0, 255, 0, 255]); // Green for 12
+    decider.set(13, [0, 0, 255, 255]); // Blue for 13
+
+    return decider;
+}
